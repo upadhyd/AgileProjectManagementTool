9b7813a85ed528010158cda5982c4b920afba881
upadhyd <upadhyd@gmail.com>
2019-02-18 13:36:21 -0500

    Addig new exception for Project identifier. Setting up @ControllerAdvice

--- src/main/java/com/dbu/ppmtool/services/ProjectService.java
+++ src/main/java/com/dbu/ppmtool/services/ProjectService.java
@@ -4,30 +4,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import com.dbu.ppmtool.domain.Project;
+import com.dbu.ppmtool.exceptions.ProjectIdentifierException;
 import com.dbu.ppmtool.repositories.ProjectRepository;
 
 /**
- * Why create a service layer?
- * We have seen people auto-wiring repository straight into controllers.
- * Good for small simple service or game. 
- * But then you end up putting a lot of logic into the controller which compromises readeability and causes code smell.
- * You always to have your logic abstracted from controller as much as possible.
- * You want your controller to be just a router as much as possible.
+ * Why create a service layer? We have seen people auto-wiring repository straight into controllers.
+ * Good for small simple service or game. But then you end up putting a lot of logic into the
+ * controller which compromises readeability and causes code smell. You always to have your logic
+ * abstracted from controller as much as possible. You want your controller to be just a router as
+ * much as possible.
+ * 
  * @author Dhaivat
  *
  */
 
 @Service
 public class ProjectService {
-  
+
   @Autowired
   private ProjectRepository projectRepository;
-  
+
   public Project saveOrUpdateProject(Project project) {
-    
-    //Logic
-    
-    return projectRepository.save(project);
+
+    try {
+      project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+      return projectRepository.save(project);
+    } catch (Exception e) {
+      throw new ProjectIdentifierException("Project Identifier '"
+          + project.getProjectIdentifier().toUpperCase() + "' already exists!");
+
+    }
+
+
   }
 
 }
