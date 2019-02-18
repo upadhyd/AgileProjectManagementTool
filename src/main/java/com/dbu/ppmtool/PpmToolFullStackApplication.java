1d72ac9d67529c94bbf4c304fde1dffa87b14781
upadhyd <upadhyd@gmail.com>
2019-02-18 10:16:08 -0500

    Project Entity And Repository

--- src/main/java/com/dbu/ppmtool/PpmToolFullStackApplication.java
+++ src/main/java/com/dbu/ppmtool/PpmToolFullStackApplication.java
@@ -1,4 +1,4 @@
-package ppmTool;
+package com.dbu.ppmtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PpmToolFullStackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PpmToolFullStackApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(PpmToolFullStackApplication.class, args);
+  }
 }
