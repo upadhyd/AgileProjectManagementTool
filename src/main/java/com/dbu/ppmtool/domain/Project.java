404141c649c008261aecca37daaf98f754786c20
upadhyd <upadhyd@gmail.com>
2019-02-18 11:12:48 -0500

    Initial project object validation

--- src/main/java/com/dbu/ppmtool/domain/Project.java
+++ src/main/java/com/dbu/ppmtool/domain/Project.java
@@ -1,107 +1,118 @@
 package com.dbu.ppmtool.domain;
 
 import java.util.Date;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import org.springframework.context.annotation.Description;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
 @Entity
 public class Project {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long Id;
-	
-	private String projectName;
-	private String projectIdentifier;
-	private String description;
-	private Date start_date;
-	private Date end_date;
-	
-	private Date created_At;
-	private Date updated_At;
-	
-	public Project() {
-		
-	}
-	
-	@PrePersist
-	protected void onCreate() {
-		this.created_At = new Date();
-	}
-	
-	@PreUpdate
-	protected void onUpdate() {
-		this.updated_At = new Date();
-	}
 
-	public Long getId() {
-		return Id;
-	}
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long Id;
+  @NotBlank(message = "Project Name is required")
+  private String projectName;
+  @NotBlank(message = "Project Identifier is required")
+  @Size(min=4, max=5, message = "Please use 4 to 5 characters")
+  @Column(updatable = false, unique = true)
+  private String projectIdentifier;
+  @NotBlank(message = "Project Description is required")
+  private String description;
+  @JsonFormat(pattern = "yyyy-mm-dd")
+  private Date start_date;
+  @JsonFormat(pattern = "yyyy-mm-dd")
+  private Date end_date;
+  @JsonFormat(pattern = "yyyy-mm-dd")
+  private Date created_At;
+  @JsonFormat(pattern = "yyyy-mm-dd")
+  private Date updated_At;
 
-	public void setId(Long id) {
-		Id = id;
-	}
+  public Project() {
 
-	public String getProjectName() {
-		return projectName;
-	}
+  }
 
-	public void setProjectName(String projectName) {
-		this.projectName = projectName;
-	}
+  public Long getId() {
+    return Id;
+  }
 
-	public String getProjectIdentifier() {
-		return projectIdentifier;
-	}
+  public void setId(Long id) {
+    Id = id;
+  }
 
-	public void setProjectIdentifier(String projectIdentifier) {
-		this.projectIdentifier = projectIdentifier;
-	}
+  public String getProjectName() {
+    return projectName;
+  }
 
-	public String getDescription() {
-		return description;
-	}
+  public void setProjectName(String projectName) {
+    this.projectName = projectName;
+  }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+  public String getProjectIdentifier() {
+    return projectIdentifier;
+  }
 
-	public Date getStart_date() {
-		return start_date;
-	}
+  public void setProjectIdentifier(String projectIdentifier) {
+    this.projectIdentifier = projectIdentifier;
+  }
 
-	public void setStart_date(Date start_date) {
-		this.start_date = start_date;
-	}
+  public String getDescription() {
+    return description;
+  }
 
-	public Date getEnd_date() {
-		return end_date;
-	}
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-	public void setEnd_date(Date end_date) {
-		this.end_date = end_date;
-	}
+  public Date getStart_date() {
+    return start_date;
+  }
 
-	public Date getCreated_At() {
-		return created_At;
-	}
+  public void setStart_date(Date start_date) {
+    this.start_date = start_date;
+  }
 
-	public void setCreated_At(Date created_At) {
-		this.created_At = created_At;
-	}
+  public Date getEnd_date() {
+    return end_date;
+  }
 
-	public Date getUpdated_At() {
-		return updated_At;
-	}
+  public void setEnd_date(Date end_date) {
+    this.end_date = end_date;
+  }
 
-	public void setUpdated_At(Date updated_At) {
-		this.updated_At = updated_At;
-	}
+  public Date getCreated_At() {
+    return created_At;
+  }
 
-	
+  public void setCreated_At(Date created_At) {
+    this.created_At = created_At;
+  }
+
+  public Date getUpdated_At() {
+    return updated_At;
+  }
+
+  public void setUpdated_At(Date updated_At) {
+    this.updated_At = updated_At;
+  }
+
+  @PrePersist
+  protected void onCreate() {
+    this.created_At = new Date();
+  }
+
+  @PreUpdate
+  protected void onUpdate() {
+    this.updated_At = new Date();
+  }
+
 }
